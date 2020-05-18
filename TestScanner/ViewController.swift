9c4f786299a4bfd51bbe42e5f39d914c4e7770c4
Jarakys <tarakakuc@gmail.com>
2020-05-18 18:59:47 +0300

    Initial Commit

---
+++ TestScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestScanner
+//
+//  Created by Kirill on 18.05.2020.
+//  Copyright © 2020 Samax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
